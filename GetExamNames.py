222c6871d4a7505a816862e2d964807d842dcd04
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-06-25 15:39:29 +0200

    update
    now returns exam name and version

--- GetExamNames.py
+++ GetExamNames.py
@@ -9,17 +9,13 @@ import pandas as pd
 
 #================
 #Get unique exam names
-#Returns list
+#Returns list in format [exam name, version]
 #================
 
 def GetExamNames(filename):
-    data = pd.read_csv(filename, sep=';', usecols=[0])
-    #get unique values
-    exams = data[data.columns.values.tolist()[0]].unique().tolist()
-    #uppercase all results
-    for i in range(len(exams)):
-        exams[i]=exams[i].upper()
-    #remove duplicates
-    exams=list(set(exams))
-    exams.sort()
+    data = pd.read_csv(filename, sep=';', usecols=[0,1])
+    data = data.apply(lambda x: x.astype(str).str.upper())
+    exams = data.drop_duplicates()
+    exams=exams.sort_values(by=exams.columns[0])
+    exams = exams.values.tolist()
     return exams

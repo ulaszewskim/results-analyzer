17bc12df25a04442f29a7d844b7b63548c19d69f
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-09 15:50:59 +0200

    checks what best students chosed

--- check_for_better_answer.py
+++ check_for_better_answer.py
@@ -11,52 +11,45 @@ from operator import itemgetter
 #Get minimum points to be in top 15%
 #returns minimum points
 #============
-def BestMinPoints(answers, results):
-    
+def best_min_points(answers, results):
     distribution_of_point = {}
     for a in range(len(answers[0])-1):
         distribution_of_point[a]=0
-    
     for z in range(len(results)):
         index = results[z][-2]
         distribution_of_point[index] +=1
-    
     top_students = int(len(answers)*0.15)
-    
     students = 0
     min_points = len(answers[0])-2
-    
     while students<top_students:
         students = students + distribution_of_point[min_points]
         min_points = min_points-1
-        
     return min_points+1
-
 
 #============
 #Check if most of top 15% students answered different than correct answer
 #returns list: [True/False, correct_answer, most_answered]
 #============
-
-def CheckForBetterAnswer(min_points, question_number, answers, results, key):
+def check_for_better_answer(min_points, question_number, answers, results, key, multiple_sign, all_correct):
+    min_points = best_min_points(answers, results)
     distibution_of_answers_best= {
             'A':0,
             'B':0,
             'C':0,
             'D':0
-    }
+            }
     correct_answer = key[question_number+1]
-    
-    for a in range(len(answers)):
-        q = answers[a][question_number+1]
-        if q in distibution_of_answers_best and results[a][-2]>=min_points:
-            distibution_of_answers_best[q]+=1
-    
-    most_answered = max(distibution_of_answers_best.items(), key=itemgetter(1))[0]
-    
-    if correct_answer == most_answered:
+    if multiple_sign.upper() in correct_answer or correct_answer == all_correct.upper():
         needs_check = False
+        most_answered = all_correct
     else:
-        needs_check = True
-    
+        for a in range(len(answers)):
+            q = answers[a][question_number+1]
+            if q in distibution_of_answers_best and results[a][-2]>=min_points:
+                distibution_of_answers_best[q]+=1
+        most_answered = max(distibution_of_answers_best.items(), key=itemgetter(1))[0]
+        if correct_answer == most_answered:
+            needs_check = False
+        else:
+            needs_check = True
     return [needs_check, correct_answer, most_answered]

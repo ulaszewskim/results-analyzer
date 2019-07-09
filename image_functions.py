f26fb22d8b440f2eb600354b2c487a97a1239df9
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-09 15:54:46 +0200

    image functions

--- image_functions.py
+++ image_functions.py
@@ -7,15 +7,12 @@ github: https://github.com/ulaszewskim
 
 from PIL import Image
 
-
-
 #================
 #Deletes white borders from image 
 #input: path to imagefile
 #overwrites inputfile
 #================
-
-def CropEmptySpace(imagefile, croppedimage):
+def crop_empty_space(imagefile, croppedimage):
     image = Image.open(imagefile)
     cropsize=[image.size[0], image.size[1],0,0]
     w_minmax = []
@@ -29,7 +26,6 @@ def CropEmptySpace(imagefile, croppedimage):
     image = image.crop(cropsize)
     image.save(croppedimage)
 
-
 #================
 #Merges two images verticaly
 #input:
@@ -37,19 +33,15 @@ def CropEmptySpace(imagefile, croppedimage):
 #    distance - width of white line separating images [pixels]
 #returns image in bytes format
 #================
-
-def MergeVertical(image1file, image2file, distance):
+def merge_vertical(image1file, image2file, distance):
     image1 = Image.open(image1file)
     image2 = Image.open(image2file)
     newimage_size = (max(image1.size[0],image2.size[0]), image1.size[1]+image2.size[1]+distance)
     newimage = Image.new('RGB', newimage_size, color=(255,255,255))
-    
     area = (0, 0, image1.size[0],image1.size[1])
     newimage.paste(image1, area)
-    
     area = (0, image1.size[1]+distance, image2.size[0], image1.size[1] + distance +image2.size[1])
     newimage.paste(image2, area)
-    
     return newimage
 
 
@@ -60,30 +52,26 @@ def MergeVertical(image1file, image2file, distance):
 #    distance - width of white line separating images [pixels]
 #returns image in bytes format
 #================
-
-def MergeHorizontal(image1file, image2file, distance):
+def merge_horizontal(image1file, image2file, distance):
     image1 = Image.open(image1file)
     image2 = Image.open(image2file)
     newimage_size = (image1.size[0] + image2.size[0] + distance, max(image1.size[1],image2.size[1]))
     newimage = Image.new('RGB', newimage_size, color=(255,255,255))
-    
     area = (0, 0, image1.size[0], image1.size[1])
     newimage.paste(image1, area)
-    
     area = (image1.size[0]+distance, 0, image1.size[0] + distance +image2.size[0], image2.size[1])
     newimage.paste(image2, area)
-    
     return newimage
-
 
 #================
 #Rezizes image to specific width, keeps aspect ratio
-#TO DO: RESIZE AGAIN IF HEIGHT>??
 #================
-    
-def ResizeWidth(imagefile, resizedimage, width):
+def resize_width(imagefile, resizedimage, width, max_height):
     image=Image.open(imagefile)
     new_height = width*image.size[1]/image.size[0]
+    print(new_height)
+    if new_height >= max_height:
+        width = max_height*image.size[0]/image.size[1]
+        new_height = max_height
     image.thumbnail((width,new_height), Image.BICUBIC)
     image.save(resizedimage)
-    

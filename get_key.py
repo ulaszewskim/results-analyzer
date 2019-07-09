e0b8812a123d6a4cb92fbaa3eea2effda1b261c9
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-09 15:56:11 +0200

    get key to the exam

--- get_key.py
+++ get_key.py
@@ -8,10 +8,10 @@ github: https://github.com/ulaszewskim
 #================
 #Get key to the exam
 #Input examname should be like: [Examname, Version]
-#Keys from LoadKey
+#Keys from load_keys
 #Returns list, 
 #================
-def GetKey(keys, exam):
+def get_key(keys, exam):
     for a in range(len(keys)):
         if keys[a][0] == exam[0] and keys[a][1] == exam[1]:
             key=keys[a]
